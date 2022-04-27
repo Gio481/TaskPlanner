167877b3f66b46710d3b08ddda3c75a7c30aa3ea
Giorgi <Gio.beridze33@gmail.com>
2022-04-27 16:37:25 +0400

    create data mapper interface

--- app/src/main/java/com/example/taskplanner/data/mapper/task/TaskDataMapper.kt
+++
@@ -1,7 +0,0 @@
-package com.example.taskplanner.data.mapper.task
-
-interface TaskDataMapper<TASK_DTO, TASK_DOMAIN> {
-    fun dtoListToDomainList(dto: List<TASK_DTO>): List<TASK_DOMAIN>
-    fun dtoToDomain(dto: TASK_DTO):TASK_DOMAIN
-    fun domainToDto(domain: TASK_DOMAIN): TASK_DTO
-}
