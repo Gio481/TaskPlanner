e8bc9e22d9c7d94f30b8fed394af29e5564740b6
Giorgi <Gio.beridze33@gmail.com>
2022-04-28 22:48:39 +0400

    added file where type aliases will be

---
+++ app/src/main/java/com/example/taskplanner/util/TypeAlias.kt
@@ -0,0 +1,6 @@
+package com.example.taskplanner.util
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+typealias BindingInflater<VB> = (inflater: LayoutInflater, container: ViewGroup?, attachRoot: Boolean) -> VB
