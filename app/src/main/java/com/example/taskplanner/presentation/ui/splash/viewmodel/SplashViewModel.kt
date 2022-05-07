67b44906f2581a8b942b7427587bb7a78ccc3b89
Giorgi <Gio.beridze33@gmail.com>
2022-05-07 13:56:51 +0400

    configure splash screen

--- app/src/main/java/com/example/taskplanner/presentation/ui/splash/viewmodel/SplashViewModel.kt
+++ app/src/main/java/com/example/taskplanner/presentation/ui/splash/viewmodel/SplashViewModel.kt
@@ -1,19 +1,21 @@
 package com.example.taskplanner.presentation.ui.splash.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.taskplanner.domain.usecase.splash.IsUserLoggedUseCase
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class SplashViewModel : ViewModel() {
-    fun splashAction(action: () -> Unit) {
-        viewModelScope.launch {
-            delay(SPLASH_SCREEN_DELAY)
-            action()
-        }
-    }
+class SplashViewModel(private val isUserLoggedUseCase: IsUserLoggedUseCase) : ViewModel() {
 
-    companion object {
-        private const val SPLASH_SCREEN_DELAY = 6000L
+    private val _isUserLoggedLiveData: MutableLiveData<Boolean> = MutableLiveData()
+    val isUserLoggedLiveData: LiveData<Boolean> = _isUserLoggedLiveData
+
+    fun isUserLogged() {
+        viewModelScope.launch {
+            _isUserLoggedLiveData.postValue(isUserLoggedUseCase.isUserLogged())
+        }
     }
 }
