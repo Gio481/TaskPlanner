8129ad0daa9ff24d3262bb9116fccc7bee710c9d
Giorgi <Gio.beridze33@gmail.com>
2022-05-07 14:43:47 +0400

    added fragment extension to handle gallery permission

--- app/src/main/java/com/example/taskplanner/util/extensions/FragmentExtensions.kt
+++ app/src/main/java/com/example/taskplanner/util/extensions/FragmentExtensions.kt
@@ -1,6 +1,9 @@
 package com.example.taskplanner.util.extensions
 
+import android.Manifest
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 
@@ -10,4 +13,14 @@ fun <T> Fragment.observer(liveData: LiveData<T>, observer: (data: T) -> Unit) {
 
 fun Fragment.showToast(message: String?) {
     Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+}
+
+fun Fragment.checkGalleryPermission(action: () -> Unit): ActivityResultLauncher<Array<String>> {
+    return registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { perm ->
+        if (perm[Manifest.permission.READ_EXTERNAL_STORAGE] == true &&
+            perm[Manifest.permission.WRITE_EXTERNAL_STORAGE] == true
+        ) {
+            action.invoke()
+        }
+    }
 }
