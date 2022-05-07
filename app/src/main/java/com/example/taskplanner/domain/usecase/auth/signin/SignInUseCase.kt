cb330af8f15f2c344be49be2760df6ede3a7f4d3
Giorgi <Gio.beridze33@gmail.com>
2022-05-07 12:53:16 +0400

    change auth use cases configuration

--- app/src/main/java/com/example/taskplanner/domain/usecase/auth/signin/SignInUseCase.kt
+++ app/src/main/java/com/example/taskplanner/domain/usecase/auth/signin/SignInUseCase.kt
@@ -3,5 +3,9 @@ package com.example.taskplanner.domain.usecase.auth.signin
 import com.google.firebase.auth.AuthResult
 
 interface SignInUseCase {
-    suspend fun signIn(email: String, password: String): AuthResult?
+    suspend fun signIn(
+        email: String,
+        password: String,
+        errorAction: (error: String) -> Unit,
+    ): AuthResult?
 }
