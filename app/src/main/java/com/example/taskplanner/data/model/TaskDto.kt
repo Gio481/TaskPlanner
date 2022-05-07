8aaf2a5fbddeecf4d1b0f7cd1a97a0690316717c
Giorgi <Gio.beridze33@gmail.com>
2022-05-07 12:17:11 +0400

    change progress storable type

--- app/src/main/java/com/example/taskplanner/data/model/TaskDto.kt
+++ app/src/main/java/com/example/taskplanner/data/model/TaskDto.kt
@@ -1,12 +1,14 @@
 package com.example.taskplanner.data.model
 
+import com.example.taskplanner.util.Progress
+
 data class TaskDto(
     val taskId: String? = null,
     val projectId: String? = null,
     val ownerId: String? = null,
     val title: String? = null,
     val description: String? = null,
-    val taskProgress: String? = null,
+    val taskProgress: Progress? = null,
     val startDate: Long? = null,
     val endDate: Long? = null,
 )
