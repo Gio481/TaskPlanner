c950e33bd5253772300d726a266530071297c59f
Giorgi <Gio.beridze33@gmail.com>
2022-05-07 13:02:46 +0400

    in di module added new use cases and remove needless parameters of the use case impl class

--- app/src/main/java/com/example/taskplanner/di/UseCaseModule.kt
+++ app/src/main/java/com/example/taskplanner/di/UseCaseModule.kt
@@ -4,8 +4,10 @@ import com.example.taskplanner.domain.usecase.auth.signin.SignInUseCase
 import com.example.taskplanner.domain.usecase.auth.signin.SignInUseCaseImpl
 import com.example.taskplanner.domain.usecase.auth.signup.SignUpUseCase
 import com.example.taskplanner.domain.usecase.auth.signup.SignUpUseCaseImpl
-import com.example.taskplanner.domain.usecase.home.HomeUseCase
-import com.example.taskplanner.domain.usecase.home.HomeUseCaseImpl
+import com.example.taskplanner.domain.usecase.home.project.GetProjectsUseCase
+import com.example.taskplanner.domain.usecase.home.project.GetProjectsUseCaseImpl
+import com.example.taskplanner.domain.usecase.home.user.UserUseCase
+import com.example.taskplanner.domain.usecase.home.user.UserUseCaseImpl
 import com.example.taskplanner.domain.usecase.project.create.ProjectCreatorUseCase
 import com.example.taskplanner.domain.usecase.project.create.ProjectCreatorUseCaseImpl
 import com.example.taskplanner.domain.usecase.project.details.edit_project.EditProjectUseCase
@@ -14,23 +16,24 @@ import com.example.taskplanner.domain.usecase.project.details.project_info.GetPr
 import com.example.taskplanner.domain.usecase.project.details.project_info.GetProjectInfoUseCaseImpl
 import com.example.taskplanner.domain.usecase.project.details.subtasks.SubTasksUseCase
 import com.example.taskplanner.domain.usecase.project.details.subtasks.SubTasksUseCaseImpl
+import com.example.taskplanner.domain.usecase.splash.IsUserLoggedUseCase
+import com.example.taskplanner.domain.usecase.splash.IsUserLoggedUseCaseImpl
 import com.example.taskplanner.domain.usecase.task.create.TaskCreatorUseCase
 import com.example.taskplanner.domain.usecase.task.create.TaskCreatorUseCaseImpl
 import com.example.taskplanner.domain.usecase.task.details.TaskDetailsUseCase
 import com.example.taskplanner.domain.usecase.task.details.TaskDetailsUseCaseImpl
-import com.example.taskplanner.domain.usecase.util.GetErrorMessage
-import com.example.taskplanner.domain.usecase.util.GetErrorMessageImpl
 import org.koin.dsl.module
 
 val useCaseModule = module {
-    single<GetErrorMessage> { GetErrorMessageImpl() }
-    single<SignInUseCase> { SignInUseCaseImpl(get(), get()) }
-    single<SignUpUseCase> { SignUpUseCaseImpl(get(), get()) }
-    single<HomeUseCase> { HomeUseCaseImpl(get(), get(), get()) }
-    single<EditProjectUseCase> { EditProjectUseCaseImpl(get(), get()) }
-    single<GetProjectInfoUseCase> { GetProjectInfoUseCaseImpl(get(), get()) }
-    single<SubTasksUseCase> { SubTasksUseCaseImpl(get(), get()) }
-    single<ProjectCreatorUseCase> { ProjectCreatorUseCaseImpl(get(), get()) }
-    single<TaskCreatorUseCase> { TaskCreatorUseCaseImpl(get(), get()) }
-    single<TaskDetailsUseCase> { TaskDetailsUseCaseImpl(get(), get()) }
+    single<SignInUseCase> { SignInUseCaseImpl(get()) }
+    single<SignUpUseCase> { SignUpUseCaseImpl(get()) }
+    single<EditProjectUseCase> { EditProjectUseCaseImpl(get()) }
+    single<GetProjectInfoUseCase> { GetProjectInfoUseCaseImpl(get()) }
+    single<SubTasksUseCase> { SubTasksUseCaseImpl(get()) }
+    single<ProjectCreatorUseCase> { ProjectCreatorUseCaseImpl(get()) }
+    single<TaskCreatorUseCase> { TaskCreatorUseCaseImpl(get()) }
+    single<TaskDetailsUseCase> { TaskDetailsUseCaseImpl(get()) }
+    single<GetProjectsUseCase> { GetProjectsUseCaseImpl(get()) }
+    single<UserUseCase> { UserUseCaseImpl(get(), get()) }
+    single<IsUserLoggedUseCase> { IsUserLoggedUseCaseImpl(get()) }
 }
