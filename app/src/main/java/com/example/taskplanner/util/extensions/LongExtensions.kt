e23f33c74b13ac1673c5efbf20156029e6d6d8c3
Giorgi <Gio.beridze33@gmail.com>
2022-05-07 16:02:09 +0400

    added long extensions

---
+++ app/src/main/java/com/example/taskplanner/util/extensions/LongExtensions.kt
@@ -0,0 +1,22 @@
+package com.example.taskplanner.util.extensions
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun Long.formatDate(): String {
+    val sdf = SimpleDateFormat("MMM dd yyyy", Locale.getDefault())
+    return sdf.format(this)
+}
+
+fun Long.getHours(): String {
+    val sdf = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
+    return sdf.format(this)
+}
+
+fun Long.toEndDate(endDate: Long): String {
+    return "${this.formatDate()} - ${endDate.formatDate()}"
+}
+
+fun Long.isValidDate(endDate: Long): Boolean {
+    return System.currentTimeMillis() in this..endDate
+}
