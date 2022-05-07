ebbcbbace8950f6944d2d2f5816f4ad75cc0e670
Giorgi <Gio.beridze33@gmail.com>
2022-05-07 16:09:13 +0400

    added fragment extensions date

--- app/src/main/java/com/example/taskplanner/util/extensions/FragmentExtensions.kt
+++ app/src/main/java/com/example/taskplanner/util/extensions/FragmentExtensions.kt
@@ -1,11 +1,19 @@
 package com.example.taskplanner.util.extensions
 
 import android.Manifest
+import android.view.View
+import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.util.Pair
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.LiveData
+import com.example.taskplanner.R
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
+import com.google.android.material.datepicker.MaterialDatePicker
 
 fun <T> Fragment.observer(liveData: LiveData<T>, observer: (data: T) -> Unit) {
     liveData.observe(this.viewLifecycleOwner) { observer(it) }
@@ -22,5 +30,61 @@ fun Fragment.checkGalleryPermission(action: () -> Unit): ActivityResultLauncher<
         ) {
             action.invoke()
         }
+    }
+}
+
+fun FragmentManager.pickDate(
+    startTime: Long? = null,
+    endTime: Long? = null,
+    validator: CalendarConstraints.DateValidator = DateValidatorPointForward.now(),
+    action: (startDate: Long, endDate: Long) -> Unit,
+) {
+    val constraints = CalendarConstraints.Builder()
+        .setValidator(validator).build()
+
+    val picker = with(MaterialDatePicker.Builder.dateRangePicker()) {
+        setTitleText("Select Date")
+        setCalendarConstraints(constraints)
+        if (startTime != null && endTime != null) {
+            setSelection(Pair(startTime, endTime))
+        }
+        build()
+    }
+
+    picker.addOnPositiveButtonClickListener {
+        val startDate = it.first
+        val endDate = it.second
+        action(startDate, endDate)
+    }
+    picker.addOnNegativeButtonClickListener {
+        picker.dismiss()
+    }
+    picker.show(this, null)
+}
+
+fun Fragment.inflatePopupMenu(
+    view: View,
+    todoAction: (() -> Unit)? = null,
+    inProgressAction: (() -> Unit)? = null,
+    doneAction: (() -> Unit)? = null,
+) {
+    val popupMenu = PopupMenu(requireContext(), view)
+    val inflater = popupMenu.menuInflater
+    inflater.inflate(R.menu.popup_menu, popupMenu.menu)
+    popupMenu.show()
+
+    popupMenu.setOnMenuItemClickListener {
+        when (it.itemId) {
+            R.id.todoItem -> {
+                todoAction?.invoke()
+            }
+            R.id.inProgressItem -> {
+                inProgressAction?.invoke()
+            }
+            R.id.doneItem -> {
+                doneAction?.invoke()
+            }
+        }
+        true
     }
 }
