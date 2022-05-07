8da66170888c254db2f447e41df8595bc5b5fdd1
Giorgi <Gio.beridze33@gmail.com>
2022-05-07 12:12:06 +0400

    added constants object

---
+++ app/src/main/java/com/example/taskplanner/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.taskplanner.util
+
+object Constants {
+    const val DESCRIPTION_FIELD = "description"
+    const val TITLE_FIELD = "title"
+    const val START_DATE_FIELD = "startDate"
+    const val END_DATE_FIELD = "endDate"
+}
