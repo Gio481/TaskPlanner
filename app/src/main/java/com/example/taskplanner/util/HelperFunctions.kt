c04f40a9fc6175a0d5e634871e647d4d097488ed
Giorgi <Gio.beridze33@gmail.com>
2022-05-07 16:07:39 +0400

    added helper function to calculate date between two dates

--- app/src/main/java/com/example/taskplanner/util/HelperFunctions.kt
+++ app/src/main/java/com/example/taskplanner/util/HelperFunctions.kt
@@ -1,7 +1,20 @@
 package com.example.taskplanner.util
 
 import android.os.Bundle
+import com.example.taskplanner.util.extensions.formatDate
+import com.example.taskplanner.util.extensions.getHours
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun bundle(action: Bundle.() -> Unit): Bundle {
     return Bundle().apply(action)
+}
+
+fun timerDate(start1: Long, end2: Long): Long {
+    val startDate = "${start1.formatDate()} ${System.currentTimeMillis().getHours()}"
+    val endDate = "${end2.formatDate()} 23:59:00"
+    val sdf = SimpleDateFormat("MMM dd yyyy HH:mm:ss", Locale.getDefault())
+    val start = sdf.parse(startDate)
+    val end = sdf.parse(endDate)
+    return end!!.time - start!!.time
 }
