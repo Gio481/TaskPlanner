5a7e7bb28e8ac9b10600d49fda10abf42f1cdaa6
Giorgi <Gio.beridze33@gmail.com>
2022-05-08 20:44:24 +0400

    added motion layout extension

--- app/src/main/java/com/example/taskplanner/util/extensions/ViewExtension.kt
+++ app/src/main/java/com/example/taskplanner/util/extensions/ViewExtension.kt
@@ -1,8 +1,42 @@
 package com.example.taskplanner.util.extensions
 
 import android.view.View
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.core.view.isVisible
+import com.example.taskplanner.R
 
-fun View.isVisible(visible:Boolean){
+fun View.isVisible(visible: Boolean) {
     this.isVisible = visible
+}
+
+fun MotionLayout.transitionEndAction(action: () -> Unit) {
+    this.setTransitionListener(
+        object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+            ) {}
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float,
+            ) {}
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                if (motionLayout?.currentState == R.id.start) {
+                    action()
+                }
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float,
+            ) {}
+        }
+    )
 }
