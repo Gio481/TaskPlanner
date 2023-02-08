07ccc06a79ce5ec70dab033656c1713c28727a7e
Gio481 <gio.beridze33@gmail.com>
2023-02-08 22:27:38 +0400

    rename name

--- app/src/main/java/com/example/taskplanner/data/model/ProjectDto.kt
+++ app/src/main/java/com/example/taskplanner/data/model/ProjectDto.kt
@@ -5,6 +5,7 @@ import com.example.taskplanner.util.Status
 data class ProjectDto(
     val projectId: String? = null,
     val ownerId: String? = null,
+    val ownerId2: String? = null,
     val title: String? = null,
     val description: String? = null,
     val startDate: Long? = null,
